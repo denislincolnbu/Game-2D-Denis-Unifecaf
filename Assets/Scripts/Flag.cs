5344a49b283848e591502870d3d88d470b138034
denislincolnbu <denislcdias@gmail.com>
2026-06-05 10:19:07 -0300

    Add files via upload

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Flag : MonoBehaviour
+{
+
+     public string nextLevel;
+     
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+       if (collision.gameObject.tag == "Player")
+       {
+
+       SceneManager.LoadScene(nextLevel);
+
+
+       }
+
+
+    }
+}
